8e0135d14ecab1265b2f55f8c04410b9bd005d91
Anderson Alves <andersonalves4608@gmail.com>
2019-08-19 17:15:39 -0300

    Invertendo vinculo autor e livro

--- InternetLibrary/Application/serializers.py
+++ InternetLibrary/Application/serializers.py
@@ -2,17 +2,20 @@ from rest_framework import serializers
 from .models import Author, Book
 
 
+class AuthorSerializer(serializers.ModelSerializer):
+    book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all(), many=True)
+
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
 class BookSerializer(serializers.ModelSerializer):
-    author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), many=True)
+    author = AuthorSerializer(read_only=True, many=True)
 
     class Meta:
         model = Book
         fields = ['name', 'summary', 'author']
 
 
-class AuthorSerializer(serializers.ModelSerializer):
-    book = BookSerializer(read_only=True, many=True)
 
-    class Meta:
-        model = Author
-        fields = ['name', 'book']
