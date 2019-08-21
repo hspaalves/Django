995d4d7c9723b69cfe002e1f1e3e5431514528b3
Anderson Alves <andersonalves4608@gmail.com>
2019-08-21 11:42:50 -0300

    Removendo Serializer BookByAuthor

--- InternetLibrary/Application/serializers.py
+++ InternetLibrary/Application/serializers.py
@@ -2,14 +2,6 @@ from rest_framework import serializers
 from .models import Author, Book
 
 
-class AuthorByBookSerializer(serializers.ModelSerializer):
-    author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), many=True)
-
-    class Meta:
-        model = Book
-        fields = ['name', 'author']
-
-
 class BookSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), many=True)
 
