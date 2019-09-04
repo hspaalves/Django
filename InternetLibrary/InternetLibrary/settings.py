7f911a488137d38b804c1729f538173492c81527
Anderson Alves <andersonalves4608@gmail.com>
2019-09-04 15:16:09 -0300

    Retornando paginação, obtendo 5 resultados por pagina

--- InternetLibrary/InternetLibrary/settings.py
+++ InternetLibrary/InternetLibrary/settings.py
@@ -81,6 +81,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'InternetLibrary.wsgi.application'
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
